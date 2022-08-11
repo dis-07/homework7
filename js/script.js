b51a36a7ae8eac79164a9b776ceeb3bdd235b6b8
Vlad Yefremenko <vladyefremenko@Vlads-MacBook-Pro.local>
2022-08-11 19:18:28 +0300

    added second task

--- js/script.js
+++ js/script.js
@@ -13,3 +13,16 @@
 // console.log(sumTo(5));
 // console.log(sumTo(10));
 // console.log(sumTo(7));
+
+
+//////////////      Task #2
+
+
+const sentance = "Lorem ipsum dolor sit amet";
+
+function findShort(str) {
+    return str.split(' ').reduce((a, b) => (b.length < a.length) ? b : a);
+  };
+
+  console.log(findShort('hello wordld olllllll'));
+  console.log(findShort(sentance));
