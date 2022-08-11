d2a1a16e3c15078670b2e3688ec8adc0c0f1f4c8
Vlad Yefremenko <vladyefremenko@Vlads-MacBook-Pro.local>
2022-08-11 15:44:21 +0300

    added first task

---
+++ js/script.js
@@ -0,0 +1,15 @@
+'usw strict';
+
+
+//////////////      Task #1
+
+// function sumTo (n) {
+//     if (n === 1) {
+//         return n;
+//     }
+//     return n + sumTo( n - 1);
+// }
+
+// console.log(sumTo(5));
+// console.log(sumTo(10));
+// console.log(sumTo(7));
